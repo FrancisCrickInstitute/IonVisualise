f59bd55379f8cbba9623c39c9c393ea350642d07
eschalnajmi <eschal.najmi@gmail.com>
2024-10-18 15:14:35 +0100

    feat: pca plot test

---
+++ pcaplot_test.py
@@ -0,0 +1,14 @@
+import sys
+import shutil
+from Home import load_data
+from helper_functions.pca import pca_plot
+ 
+test_filename_list = [
+    "mock_data/mock_proteomics_data.csv",
+]
+ 
+for filename in test_filename_list:
+    data_frame = load_data(filename)
+    pca_plot(data_frame)
+ 
+sys.exit(0)
