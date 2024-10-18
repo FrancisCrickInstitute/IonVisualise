82df7c62dd660bd01f77b8112a8e875dd05c86ba
Yew Mun <yewmun.yip@crick.ac.uk>
2024-10-18 10:42:46 +0100

    fix: stream data showing in every page when no data is uploaded, user unable to select which data set to visualise when multiple files are uploaded

--- Home.py
+++ Home.py
@@ -1,13 +1,21 @@
 import os
 import time
 from pathlib import Path
+
 import pandas as pd
 import streamlit as st
+
 from helper_functions import pca
 from helper_functions import scatterplot as sc
 from helper_functions import volcano_plot as vp
 
 st.set_page_config(initial_sidebar_state="expanded")
+
+# Create temporary directory if it doesn't exist to store uploaded files that are converted
+temp_dir = "temp_files"
+if not os.path.exists(temp_dir):
+    os.makedirs(temp_dir)
+
 wholepage = st.container()
 
 # Initialize session state for data, navigation, and file paths if they do not exist
@@ -21,46 +29,26 @@ if "file_paths" not in st.session_state:
     st.session_state.file_paths = []  # To store local file paths
 
 
-def save_uploaded_file(uploaded_file):
-    """Save uploaded file to a temporary directory and return the path."""
-    temp_dir = "temp_files"
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
-    file_path = os.path.join(temp_dir, uploaded_file.name)
-    with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return file_path
-
-
 def stream_data(text):
     for word in text.split(" "):
         yield word + " "
         time.sleep(0.02)
 
+
 def convert_csv(uploaded_files):
-        new_files = []
-        for file in uploaded_files:
-            if file.name.endswith(".csv"):
-                new_files.append(file)
-                continue
+    new_files = []
+    for file in uploaded_files:
+        if file.name.endswith(".xls"):
+            df = pd.read_excel(file)
 
-            if file.name.endswith(".xls"):
-                df = pd.read_excel(file)
-                
-            if file.name.endswith(".txt"):
-                df = pd.read_csv(file, delimiter='\t')
-            
-            csv_file = f"temp_files/{Path(file.name).stem}.csv"
-            
-            df.to_csv(csv_file, index=False)
-            new_files.append(csv_file)
-        return new_files
+        elif file.name.endswith(".txt"):
+            df = pd.read_csv(file, delimiter="\t")
 
-def reset_app():
-    """Reset session state and rerun the app."""
-    st.session_state.uploaded_data = []
-    st.session_state.file_paths = []
-    st.rerun()  # This will reset the app
+        csv_file = f"temp_files/{Path(file.name).stem}.csv"
+
+        df.to_csv(csv_file, index=False)
+        new_files.append(csv_file)
+    return new_files
 
 
 def homePage():
@@ -74,7 +62,7 @@ def homePage():
         insights.
         """
     wholepage.write_stream(stream_data(description))
-    
+
     # If no files are uploaded, show the file uploader
     if not st.session_state.uploaded_data:
         uploaded_files = wholepage.file_uploader(
@@ -84,26 +72,35 @@ def homePage():
         )
 
         if uploaded_files:
-            # Save files locally and store paths
-            st.session_state.uploaded_data = uploaded_files
-            st.session_state.file_paths = [
-                save_uploaded_file(file) for file in uploaded_files
-            ]
-            
             uploaded_files = convert_csv(uploaded_files)
+
+            # Save files locally and store paths
+            st.session_state.uploaded_data = [
+                Path(file).name for file in uploaded_files
+            ]
+
+            st.session_state.file_paths = [
+                f"temp_files/{Path(file).name}" for file in uploaded_files
+            ]
+
+    st.write(st.session_state.uploaded_data)
+    st.write(st.session_state.file_paths)
 
     # If files are uploaded, display them with an option to remove
     if st.session_state.uploaded_data:
         wholepage.success("Files uploaded:")
         for i, uploaded_file in enumerate(st.session_state.uploaded_data):
             col1, col2 = wholepage.columns([8, 1])
-            col1.write(uploaded_file.name)
+            col1.write(uploaded_file)
             # Add an "X" button to remove the file and reset the app
             if col2.button("X", key=f"remove_file_{i}"):
-                st.session_state.uploaded_data.pop(i)
-                st.session_state.file_paths.pop(i)
+                st.session_state.uploaded_data.remove(uploaded_file)
+                st.session_state.file_paths.remove(
+                    f"temp_files/{uploaded_file}"
+                )
+                os.remove(f"temp_files/{uploaded_file}")
                 st.rerun()
-                # reset_app()  # Reset the app if a file is removed
+
 
 def load_data(file_path):
     """Load data from the saved local file."""
@@ -120,9 +117,13 @@ def load_data(file_path):
 def PCAPage(file_paths):
     wholepage.title("_PCA_")
 
-    for file_path in file_paths:
-        wholepage.write(f":blue[{os.path.basename(file_path)}]")
-        df = load_data(file_path)
+    file_paths = [f"{Path(file).stem}" for file in file_paths]
+    selected_data = st.selectbox("Select Dataset", file_paths)
+
+    wholepage.write(f":blue[{os.path.basename(Path(selected_data).stem)}]")
+    df = load_data(f"temp_files/{selected_data}.csv")
+
+    st.dataframe(df)
 
     proteins = df.iloc[:, 0].to_numpy()
     with wholepage:
@@ -136,9 +137,11 @@ def PCAPage(file_paths):
 def volcanoPage(file_paths):
     wholepage.title("_Volcano plot_")
 
-    for file_path in file_paths:
-        wholepage.write(f":blue[{os.path.basename(file_path)}]")
-        df = load_data(file_path)
+    file_paths = [f"{Path(file).stem}" for file in file_paths]
+    selected_data = st.selectbox("Select Dataset", file_paths)
+
+    wholepage.write(f":blue[{os.path.basename(Path(selected_data).stem)}]")
+    df = load_data(f"temp_files/{selected_data}.csv")
 
     proteins = df.iloc[:, 0].to_numpy()
     with wholepage:
@@ -152,9 +155,11 @@ def volcanoPage(file_paths):
 def scatterPage(file_paths):
     wholepage.title("_Scatter plot_")
 
-    for file_path in file_paths:
-        wholepage.write(f":blue[{os.path.basename(file_path)}]")
-        df = load_data(file_path)
+    file_paths = [f"{Path(file).stem}" for file in file_paths]
+    selected_data = st.selectbox("Select Dataset", file_paths)
+
+    wholepage.write(f":blue[{os.path.basename(Path(selected_data).stem)}]")
+    df = load_data(f"temp_files/{selected_data}.csv")
 
     # Get all column names from the dataframe for selection
     column_options = df.select_dtypes(include=[float, int]).columns.tolist()
@@ -162,9 +167,27 @@ def scatterPage(file_paths):
     with wholepage:
         with st.sidebar:
             # Let the user select the X and Y axes from the dataframe columns
-            x_axis = st.selectbox("Select X-axis", column_options, index=column_options.index("Spectral_Count") if "Spectral_Count" in column_options else 0)
-            y_axis = st.selectbox("Select Y-axis", column_options, index=column_options.index("Peptide_Count") if "Peptide_Count" in column_options else 1)
-            selected_protein = st.selectbox("Select protein", df.iloc[:, 0].to_numpy())
+            x_axis = st.selectbox(
+                "Select X-axis",
+                column_options,
+                index=(
+                    column_options.index("Spectral_Count")
+                    if "Spectral_Count" in column_options
+                    else 0
+                ),
+            )
+            y_axis = st.selectbox(
+                "Select Y-axis",
+                column_options,
+                index=(
+                    column_options.index("Peptide_Count")
+                    if "Peptide_Count" in column_options
+                    else 1
+                ),
+            )
+            selected_protein = st.selectbox(
+                "Select protein", df.iloc[:, 0].to_numpy()
+            )
             st.write(f"Selected protein: {selected_protein}")
 
     # Use selected columns for the scatter plot
@@ -174,9 +197,11 @@ def scatterPage(file_paths):
 def timeseriesPage(file_paths):
     wholepage.title("_Timeseries plot_")
 
-    for file_path in file_paths:
-        wholepage.write(f":blue[{os.path.basename(file_path)}]")
-        df = load_data(file_path)
+    file_paths = [f"{Path(file).stem}" for file in file_paths]
+    selected_data = st.selectbox("Select Dataset", file_paths)
+
+    wholepage.write(f":blue[{os.path.basename(Path(selected_data).stem)}]")
+    df = load_data(f"temp_files/{selected_data}.csv")
 
     proteins = df.iloc[:, 0].to_numpy()
     with wholepage:
@@ -184,7 +209,6 @@ def timeseriesPage(file_paths):
             selected_protein = st.selectbox("Select protein", proteins)
             st.write(f"Selected protein: {selected_protein}")
 
-    # sc.scatter_with_smooth_lines(df, df.iloc[:,0], df.iloc[:,4])
 
 def main():
     nav1, nav2, nav3, nav4, nav5 = wholepage.columns(5)
@@ -202,23 +226,22 @@ def main():
         st.session_state.nav_state = "TimeSeries"
 
     # If there are no files in session state and user tries to access another page
-    if (st.session_state.nav_state in ["PCA", "Volcano", "Scatter", "TimeSeries"]) and len(
-        st.session_state.file_paths
-    ) == 0:
-        wholepage.warning("Please upload data to proceed")
-        st.session_state.nav_state = "Home"
-
-    # Navigate to the selected page based on session state
     if st.session_state.nav_state == "Home":
         homePage()
-    elif st.session_state.nav_state == "PCA":
-        PCAPage(st.session_state.file_paths)
-    elif st.session_state.nav_state == "Volcano":
-        volcanoPage(st.session_state.file_paths)
-    elif st.session_state.nav_state == "Scatter":
-        scatterPage(st.session_state.file_paths)
-    elif st.session_state.nav_state == "TimeSeries":
-        timeseriesPage(st.session_state.file_paths)
+    elif (
+        st.session_state.nav_state
+        in ["PCA", "Volcano", "Scatter", "TimeSeries"]
+    ) and len(st.session_state.file_paths) == 0:
+        wholepage.warning("Please upload data to proceed")
+    else:
+        if st.session_state.nav_state == "PCA":
+            PCAPage(st.session_state.file_paths)
+        elif st.session_state.nav_state == "Volcano":
+            volcanoPage(st.session_state.file_paths)
+        elif st.session_state.nav_state == "Scatter":
+            scatterPage(st.session_state.file_paths)
+        elif st.session_state.nav_state == "TimeSeries":
+            timeseriesPage(st.session_state.file_paths)
 
 
 main()
