b2f588b149309e17ecad507ea76a343884531b0f
eschalnajmi <eschal.najmi@gmail.com>
2024-10-18 14:32:03 +0100

    feat: convert csv test

---
+++ convertcsv_test.py
@@ -0,0 +1,30 @@
+"""Module providing a function to exit early"""
+import sys
+from Home import convert_csv
+
+test_filename_list = [
+    "mock_data/mock_proteomics_data.csv",
+    "mock_data/mock_proteomics_data.txt",
+]
+
+test_file_list = []
+for filename in test_filename_list:
+    file = open(filename, "r", encoding="utf-8")
+    test_file_list.append(file)
+print("::debug::Testing convert_csv")
+test_filename_converted_list = convert_csv(test_file_list)
+print("::notice::Success convert_csv")
+original_file = open(test_filename_list[0], "r", encoding="utf-8")
+original_file_content = original_file.read()
+original_file.close()
+print("::debug::Checking convert_csv output was correct")
+for filename in test_filename_converted_list:
+    file = open(filename, "r", encoding="utf-8")
+    file_content = file.read()
+    if file_content != original_file_content:
+        print("::error::Success convert_csv output was incorrect")
+        sys.exit(1)
+
+    file.close()
+print("::notice::Success convert_csv output was correct")
+sys.exit(0)
